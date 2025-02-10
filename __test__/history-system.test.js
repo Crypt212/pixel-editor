a16a1d4d3ea6124994db52e6c4a640a0a2cb2b5f
Ahmed El-Esseily <missing_colon_@outlook.com>
2025-02-10 17:21:41 +0200

    Add HistorySystem class and initial tests
    - Implement HistorySystem class with methods for managing unit groups of
      actions, which allows doing undo/redo operations between groups,
      adding new action groups, retrieving action data ... etc.
    - Add Jest tests to cover construction, functionality, and edge cases.

---
+++ __test__/history-system.test.js
@@ -0,0 +1,354 @@
+import HistorySystem from "../scripts/history-system.js";
+
+describe("HistorySystem", () => {
+    let hs;
+    describe("Construction", () => {
+        test("Should throw an error when capacity is not defined, finite integer", () => {
+            expect(() => new HistorySystem()).toThrow(
+                "Capacity must be defined",
+            );
+
+            expect(() => new HistorySystem([])).toThrow(
+                "Capacity must be a finite integer number",
+            );
+            expect(() => new HistorySystem(false)).toThrow(
+                "Capacity must be a finite integer number",
+            );
+
+            expect(() => new HistorySystem(Infinity)).toThrow(
+                "Capacity must be a finite integer number",
+            );
+            expect(() => new HistorySystem(NaN)).toThrow(
+                "Capacity must be a finite integer number",
+            );
+
+            expect(() => new HistorySystem(0.8)).toThrow(
+                "Capacity must be a finite integer number",
+            );
+            expect(() => new HistorySystem(13.001)).toThrow(
+                "Capacity must be a finite integer number",
+            );
+        });
+
+        test("Should throw an error when capacity is not between 1 and 64", () => {
+            expect(() => new HistorySystem(-20)).toThrow(
+                "Capacity must be between 1 and 64 inclusive",
+            );
+            expect(() => new HistorySystem(0)).toThrow(
+                "Capacity must be between 1 and 64 inclusive",
+            );
+            expect(() => new HistorySystem(100)).toThrow(
+                "Capacity must be between 1 and 64 inclusive",
+            );
+        });
+
+        test("Should return capacity of buffer when calling getBufferCapacity", () => {
+            hs = new HistorySystem(1);
+            expect(hs.getBufferCapacity).toBe(1);
+
+            hs = new HistorySystem(20);
+            expect(hs.getBufferCapacity).toBe(20);
+
+            hs = new HistorySystem(64);
+            expect(hs.getBufferCapacity).toBe(64);
+        });
+    });
+
+    describe("Functionality", () => {
+        let hs;
+        let assertGroup;
+        beforeEach(() => {
+            assertGroup = function(
+                offset,
+                expectedID = null,
+                expectedName = null,
+                expectedData = null,
+            ) {
+                if (expectedID !== null)
+                    expect(hs.getActionGroupID(offset)).toBe(expectedID);
+                if (expectedName !== null)
+                    expect(hs.getActionGroupName(offset)).toBe(expectedName);
+                if (expectedData !== null) {
+                    expect(hs.getActionData(offset)).toEqual(expectedData);
+                    expect(hs.getActionData(offset)).not.toBe(expectedData);
+                }
+            };
+            hs = new HistorySystem(5);
+        });
+
+        describe("Adding action group - Undoing/Redoing", () => {
+            test("Should throw an error if given action group name is not a string", () => {
+                expect(() => hs.addActionGroup(0)).toThrow(
+                    "Action group name must be string",
+                );
+            });
+
+            test("Should add action group with given name or no name given if none given", () => {
+                expect(() => hs.addActionGroup("")).not.toThrow();
+                expect(() => hs.addActionGroup("ahmed")).not.toThrow();
+                expect(() => hs.addActionGroup()).not.toThrow();
+                assertGroup(0, 2, "");
+                assertGroup(-1, 1, "ahmed");
+                assertGroup(-2, 0, "");
+            });
+
+        test("Should undo and redo and return action group ID, -1 if at start", () => {
+            assertGroup(0, -1, -1);
+            
+            hs.addActionGroup("AG1");
+
+            assertGroup(0, 0, "AG1");
+
+            hs.addActionGroup("AG2");
+
+            assertGroup(0, 1, "AG2");
+
+            hs.addActionGroup("AG3");
+
+            assertGroup(0, 2, "AG3");
+
+            expect(hs.undo()).toBe(1);
+
+            assertGroup(0, 1, "AG2");
+
+            expect(hs.undo()).toBe(0);
+
+            assertGroup(0, 0, "AG1");
+
+            expect(hs.undo()).toBe(-1);
+
+            assertGroup(0, -1, -1);
+
+            expect(hs.undo()).toBe(-1);
+
+            assertGroup(0, -1, -1);
+
+            expect(hs.redo()).toBe(0);
+
+            assertGroup(0, 0, "AG1");
+
+            expect(hs.redo()).toBe(1);
+
+            assertGroup(0, 1, "AG2");
+
+            expect(hs.redo()).toBe(2);
+
+            assertGroup(0, 2, "AG3");
+
+            expect(hs.redo()).toBe(2);
+
+            assertGroup(0, 2, "AG3");
+        });
+
+            test("Should truncate the undid part if added new action group before it", () => {
+                expect(() => hs.addActionGroup("wash")).not.toThrow();
+                expect(() => hs.addActionGroup("do whatever")).not.toThrow();
+                expect(() => hs.addActionGroup("sleep")).not.toThrow();
+                expect(() => hs.addActionGroup("play")).not.toThrow();
+                expect(() => hs.undo()).not.toThrow();
+                expect(() => hs.undo()).not.toThrow();
+                assertGroup(1, 2, "sleep");
+                assertGroup(2, 3, "play");
+                expect(() => hs.addActionGroup("study")).not.toThrow();
+                assertGroup(1, -1, -1);
+                assertGroup(2, -1, -1);
+            });
+
+            test("Should override old groups if added new groups while buffer is full", () => {
+                expect(hs.getBufferSize).toBe(0);
+                expect(() => hs.addActionGroup("wash")).not.toThrow();
+                expect(() => hs.addActionGroup("do whatever")).not.toThrow();
+                expect(() => hs.addActionGroup("sleep")).not.toThrow();
+                expect(() => hs.addActionGroup("play")).not.toThrow();
+                expect(() => hs.addActionGroup("study")).not.toThrow();
+                expect(() => hs.addActionGroup("run")).not.toThrow();
+                expect(() => hs.addActionGroup("eat")).not.toThrow();
+                expect(hs.getBufferSize).toBe(5);
+
+                assertGroup(0, 6, "eat");
+                assertGroup(-1, 5, "run");
+                assertGroup(-2, 4, "study");
+                assertGroup(-3, 3, "play");
+                assertGroup(-4, 2, "sleep");
+                assertGroup(-5, -1, -1);
+                assertGroup(1, -1, -1);
+            });
+
+            test("Should work on correctly edge-cases and query currectly", () => {
+                expect(hs.getBufferSize).toBe(0);
+                expect(() => hs.addActionGroup("wash")).not.toThrow();
+                expect(hs.getBufferSize).toBe(1);
+                expect(() => hs.addActionGroup("do whatever")).not.toThrow();
+                expect(() => hs.addActionGroup("sleep")).not.toThrow();
+                expect(() => hs.addActionGroup("play")).not.toThrow();
+                expect(hs.getBufferSize).toBe(4);
+
+                assertGroup(-1, 2, "sleep");
+                assertGroup(-2, 1, "do whatever");
+                assertGroup(-3, 0, "wash");
+                assertGroup(-4, -1, -1);
+
+                expect(() => hs.addActionGroup("study")).not.toThrow();
+                expect(() => hs.addActionGroup("run")).not.toThrow();
+                expect(() => hs.addActionGroup("eat")).not.toThrow();
+                expect(hs.getBufferSize).toBe(5);
+
+                assertGroup(0, 6, "eat");
+                assertGroup(-1, 5, "run");
+                assertGroup(-2, 4, "study");
+                assertGroup(-3, 3, "play");
+                assertGroup(-4, 2, "sleep");
+                assertGroup(-5, -1, -1);
+                assertGroup(1, -1, -1);
+
+                expect(() => hs.undo()).not.toThrow();
+                expect(() => hs.undo()).not.toThrow();
+                expect(() => hs.undo()).not.toThrow();
+                expect(hs.getBufferSize).toBe(5);
+
+                assertGroup(0, 3, "play");
+
+                expect(() => hs.addActionGroup("action1")).not.toThrow();
+                expect(() => hs.addActionGroup("action2")).not.toThrow();
+                expect(() => hs.addActionGroup("action3")).not.toThrow();
+                expect(() => hs.addActionGroup("action4")).not.toThrow();
+                expect(hs.getBufferSize).toBe(5);
+
+                assertGroup(-6, -1, -1);
+                assertGroup(-5, -1, -1);
+                assertGroup(-4, 3, "play");
+                assertGroup(-3, 7, "action1");
+                assertGroup(-2, 8, "action2");
+                assertGroup(-1, 9, "action3");
+                assertGroup(0, 10, "action4");
+                assertGroup(1, -1, -1);
+                assertGroup(2, -1, -1);
+
+                expect(() => hs.undo()).not.toThrow();
+                expect(() => hs.undo()).not.toThrow();
+                expect(() => hs.undo()).not.toThrow();
+                expect(() => hs.undo()).not.toThrow();
+                expect(() => hs.undo()).not.toThrow();
+                expect(hs.getBufferSize).toBe(5);
+
+                assertGroup(-1, -1, -1);
+                assertGroup(0, -1, -1);
+                assertGroup(1, 3, "play");
+                assertGroup(2, 7, "action1");
+                assertGroup(3, 8, "action2");
+                assertGroup(4, 9, "action3");
+                assertGroup(5, 10, "action4");
+                assertGroup(6, -1, -1);
+                assertGroup(7, -1, -1);
+
+                expect(() => hs.redo()).not.toThrow();
+                expect(hs.getBufferSize).toBe(5);
+
+                assertGroup(-2, -1, -1);
+                assertGroup(-1, -1, -1);
+                assertGroup(0, 3, "play");
+                assertGroup(1, 7, "action1");
+                assertGroup(2, 8, "action2");
+                assertGroup(3, 9, "action3");
+                assertGroup(4, 10, "action4");
+                assertGroup(5, -1, -1);
+                assertGroup(6, -1, -1);
+
+                expect(hs.getBufferSize).toBe(5);
+            });
+            test("Should undo and redo currectly and return id of action group that was redid/undid to, -1 if undid to the start", () => {
+            });
+        });
+        describe("Adding action data to action groups", () => {
+            test("Should throw an error if added action data without specifying action group, else adds to the specified group", () => {
+                expect(() => hs.addActionData("4")).toThrow(
+                    "No action group to add to",
+                );
+                expect(() => hs.addActionGroup()).not.toThrow();
+                expect(() => hs.addActionGroup()).not.toThrow();
+                expect(() => hs.addActionData("4")).not.toThrow();
+                expect(() => hs.undo()).not.toThrow();
+                expect(() => hs.addActionData("4")).not.toThrow();
+                expect(() => hs.undo()).not.toThrow();
+                expect(() => hs.addActionData("4")).toThrow(
+                    "No action group to add to",
+                );
+            });
+            test("Should do shallow copy if data is object or array or primitive data", () => {
+                let arrExample = [4, [3, 5, 2], 5];
+                expect(() => hs.addActionGroup()).not.toThrow();
+                expect(() => hs.addActionData("4")).not.toThrow();
+                expect(() => hs.addActionData(4)).not.toThrow();
+                expect(() => hs.addActionData([4, 3, 5])).not.toThrow();
+                expect(() => hs.addActionData(arrExample)).not.toThrow();
+                expect(() =>
+                    hs.addActionData({ a: 4, b: [3, 5, 2], c: 5 }),
+                ).not.toThrow();
+                assertGroup(0, 0, "", [
+                    "4",
+                    4,
+                    [4, 3, 5],
+                    [4, [3, 5, 2], 5],
+                    { a: 4, b: [3, 5, 2], c: 5 },
+                ]);
+                arrExample[1][1] = [1, 2];
+                assertGroup(0, 0, "", [
+                    "4",
+                    4,
+                    [4, 3, 5],
+                    [4, [3, [1, 2], 2], 5],
+                    { a: 4, b: [3, 5, 2], c: 5 },
+                ]);
+            });
+            test("Should throw an error if given action group name is not a string", () => {
+                expect(() => hs.addActionGroup(0)).toThrow(
+                    "Action group name must be string",
+                );
+            });
+            test("Should add action group with given name or no name given if none given", () => {
+                expect(() => hs.addActionGroup("")).not.toThrow();
+                expect(() => hs.addActionGroup("ahmed")).not.toThrow();
+                expect(() => hs.addActionGroup()).not.toThrow();
+                assertGroup(0, 2, "");
+                assertGroup(-1, 1, "ahmed");
+                assertGroup(-2, 0, "");
+            });
+        });
+
+        describe("Getting size and capacity", () => {
+            test("Should throw an exception if offset is not a finite integer", () => {
+                let toTest = (offset, expectedThrow) => {
+                    if (expectedThrow === undefined) {
+                        expect(() => hs.getActionGroupID(offset)).not.toThrow();
+                        expect(() =>
+                            hs.getActionGroupName(offset),
+                        ).not.toThrow();
+                        expect(() => hs.getActionData(offset)).not.toThrow();
+                    } else {
+                        expect(() => hs.getActionGroupID(offset)).toThrow(
+                            expectedThrow,
+                        );
+                        expect(() => hs.getActionGroupName(offset)).toThrow(
+                            expectedThrow,
+                        );
+                        expect(() => hs.getActionData(offset)).toThrow(
+                            expectedThrow,
+                        );
+                    }
+                };
+                toTest(0);
+                toTest(-10);
+                toTest(100);
+                toTest("a", "Offset must be a finite integer number");
+                toTest("aah", "Offset must be a finite integer number");
+                toTest(["aah"], "Offset must be a finite integer number");
+                toTest([3], "Offset must be a finite integer number");
+                toTest(
+                    { a: 3, b: 4 },
+                    "Offset must be a finite integer number",
+                );
+            });
+        });
+
+    });
+});
---
+++ scripts/history-system.js
@@ -0,0 +1,215 @@
+class HistorySystem {
+    #buffer;
+    #currentIndex = -1;
+    #startIndex = 0;
+    #endIndex = -1;
+    #actionGroupIDCounter = -1;
+
+    /**
+     * Represents a history system to store, undo, redo action data.
+     * @constructor
+     * @param {number} capacity - The max size of the history buffer (range from 1 to 64)
+     */
+    constructor(capacity) {
+        if (capacity === undefined) throw new Error("Capacity must be defined");
+
+        if (!Number.isInteger(capacity) || !Number.isFinite(capacity))
+            throw new Error("Capacity must be a finite integer number");
+
+        if (!(1 <= capacity && capacity <= 64))
+            throw new Error("Capacity must be between 1 and 64 inclusive");
+
+        capacity = Math.floor(capacity);
+        this.#buffer = new Array(capacity);
+    }
+
+    /**
+     * Addes action group with given name and incremental ID to the action buffer
+     * @method
+     * @param {string} actionGroupName - Name of the action group to be added
+     * @throws {Error} Throws an error if given action group name is not of type string
+     */
+    addActionGroup(actionGroupName = "") {
+        if (typeof actionGroupName !== "string")
+            throw new Error("Action group name must be string");
+
+        if (this.getBufferSize === this.getBufferCapacity) {
+            this.#startIndex = (this.#startIndex + 1) % this.getBufferCapacity;
+        }
+
+        if (this.#currentIndex === -1) this.#currentIndex = this.#startIndex;
+        else
+            this.#currentIndex =
+                (this.#currentIndex + 1) % this.getBufferCapacity;
+
+        this.#endIndex = this.#currentIndex;
+
+        this.#buffer[this.#currentIndex] = {
+            groupName: actionGroupName,
+            groupID: ++this.#actionGroupIDCounter,
+            actionData: [],
+        };
+    }
+
+    /**
+     * Adds a shallow copy of action data to the current selected action group
+     * @method
+     * @param {any} actionDataObject - The action data to be stored in the current action group
+     * @throws {Error} Throws an error if no action group selected currently (eg. when at the absolute start)
+     */
+    addActionData(actionDataObject) {
+        if (this.#currentIndex === -1) {
+            throw new Error("No action group to add to.");
+        }
+
+        let newObject;
+        if (typeof actionDataObject === "object" && actionDataObject !== null) {
+            if (Array.isArray(actionDataObject))
+                newObject = [...actionDataObject];
+            else newObject = { ...actionDataObject };
+        } else newObject = actionDataObject;
+
+        this.#buffer[this.#currentIndex].actionData.push(newObject);
+    }
+
+    /**
+     * Retrieves action group at an offset from current selected group
+     * @method
+     * @param {number} offset - The the offset from the current group for which ID gets returned
+     * @returns {Object{groupID, groupName, actionArray} | number} The selected action group, -1 if out of range
+     * @throws {Error} Throws an error if offset is not an integer number
+     */
+    #getActionGroup(offset = 0) {
+        if (!Number.isInteger(offset) || !Number.isFinite(offset))
+            throw new Error("Offset must be a finite integer number");
+
+        let check;
+        let index = this.#currentIndex;
+
+        if (offset > 0) {
+            if (index === -1) {
+                index = this.#startIndex;
+                offset--;
+            }
+
+            check = this.#endIndex - index;
+            check = check < 0 ? check + this.getBufferCapacity : check;
+
+            if (check - offset < 0) return -1;
+
+            return this.#buffer[(index + offset) % this.getBufferCapacity];
+        } else if (offset < 0) {
+            offset *= -1;
+
+            if (index === -1) return -1;
+
+            check = index - this.#startIndex;
+            check = check < 0 ? check + this.getBufferCapacity : check;
+
+            // if (check - offset) is -1 => result index at -1 which is start => return -1
+            if (check - offset < 0) return -1;
+
+            return this.#buffer[
+                (index - offset + this.getBufferCapacity) %
+                this.getBufferCapacity
+            ];
+        } else {
+            if (this.#currentIndex === -1) return -1;
+            return this.#buffer[this.#currentIndex];
+        }
+    }
+
+    /**
+     * Retrieves action group ID at an offset from current selected group
+     * @method
+     * @param {number} offset - The the offset from the current group for which ID gets returned
+     * @returns {number} The action group ID, or -1 if not in range.
+     */
+    getActionGroupID(offset = 0) {
+        let group = this.#getActionGroup(offset);
+        if (group === -1) return -1;
+        return group.groupID;
+    }
+
+    /**
+     * Retrieves action group name at an offset from current selected group
+     * @method
+     * @param {number} offset - The the offset from the current group for which name gets returned
+     * @returns {string | number} The action group name, or -1 if not in range.
+     */
+    getActionGroupName(offset = 0) {
+        let group = this.#getActionGroup(offset);
+        if (group === -1) return -1;
+        return group.groupName;
+    }
+
+    /**
+     * Retrieves action group data at an offset from current selected group
+     * @method
+     * @param {number} offset - The the offset from the current group for which data gets returned
+     * @returns {Array | number} An array containing the action group data, or -1 if not in range
+     */
+    getActionData(offset = 0) {
+        let group = this.#getActionGroup(offset);
+        if (group === -1) return -1;
+        return group.actionData;
+    }
+
+    /**
+     * Reverts to the previous action group
+     * @method
+     * @returns {number} The ID of the previous action group
+     */
+    undo() {
+        if (this.#currentIndex === this.#startIndex) this.#currentIndex = -1;
+
+        if (this.#currentIndex === -1) return this.#currentIndex;
+
+        this.#currentIndex =
+            (this.#currentIndex - 1 + this.getBufferCapacity) %
+            this.getBufferCapacity;
+        return this.#buffer[this.#currentIndex].groupID;
+    }
+
+    /**
+     * Advances to the next action group
+     * @method
+     * @returns {number} The ID of next action group
+     */
+    redo() {
+        if (this.#currentIndex !== this.#endIndex)
+            if (this.#currentIndex === -1) {
+                this.#currentIndex = this.#startIndex;
+            } else {
+                this.#currentIndex =
+                    (this.#currentIndex + 1) % this.getBufferCapacity;
+            }
+
+        return this.#buffer[this.#currentIndex].groupID;
+    }
+
+    /**
+     * Retrieves number of action groups stored currently in the action buffer
+     * @method
+     * @returns {number} The size
+     */
+    get getBufferSize() {
+        if (this.#endIndex == -1) return 0;
+        return (
+            ((this.#endIndex - this.#startIndex + this.getBufferCapacity) %
+                this.getBufferCapacity) +
+            1
+        );
+    }
+
+    /**
+     * Retrieves action buffer capacity (maximum number of action groups to add to the system)
+     * @method
+     * @returns {number} The capacity
+     */
+    get getBufferCapacity() {
+        return this.#buffer.length;
+    }
+}
+
+export default HistorySystem;
